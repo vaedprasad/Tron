ccedbf2a2ab1e766fd1c8b6746aa12248937b570
vaedprasad <VaedPrasad@Vaeds-MBP.home>
2016-05-26 22:34:40 -0400

    Merge Wall-Hugging into Java-Bot + Remove Mini-Max Comments

--- Java-Bot/TronBot.java
+++ Java-Bot/TronBot.java
@@ -14,22 +14,70 @@ import java.io.*;
 public class TronBot {
     private static int[] myPos = new int[2];
     private static ArrayList<ArrayList<Tron.Tile>> map;
-
+    /*
     public static void main(String[] args) {
-        Tron.init();        // Call this to initialize networking and the debug file
-        // We can't print to console, because that channel is used for IO with the environment.
-        // We therefore print to either debug1.log or debug2.log. The first is for player1, the second for player two
-        // Use log() rather than print.
+        Tron.init();
         while (true) {         // Execute loop forever (or until game ends)
             map = Tron.getMap();
             ArrayList<Tron.Direction> goods = new ArrayList<Tron.Direction>();
             myPos = TronUtils.findMe(map);
             moveFirstFree();
         }
-    }
-
-    public static void moveFirstFree() {
-        boolean moveFound = false;
+    }*/
+    
+    public static void main(String[] args) {
+        Tron.init();
+        int count = 0;
+        
+        while (true) {
+            count++;
+           
+            ArrayList<ArrayList<Tron.Tile>> mList = Tron.getMap();
+            int[][] m = new int[mList.size()][mList.get(0).size()];
+            for (int i = 0; i < mList.size(); i++) {
+                for (int j = 0; j < mList.get(i).size(); j++) {
+                    m[i][j] = mList.get(i).get(j).ordinal();
+                }
+            }
+            
+            int myLocX = 0, myLocY = 0;
+            for(int y = 0; y < 16; y++)  {
+                for(int x = 0; x < 16; x++) {
+                    if(Tron.Tile.values()[m[y][x]] == Tron.Tile.ME) {
+                        myLocX = x;
+                        myLocY = y;
+                    }
+                }
+            }
+            
+            boolean [] safe = emptyAdjacentSquares(m, myLocX, myLocY);
+            
+            int [] dirEmptyCount = new int[4];
+            for(int a = 0; a < 4; a++) {
+                if(safe[a]) {
+                    int[] possibleSquare = getLocation(myLocX, myLocY, a);
+                    if(possibleSquare[0] != -1 && possibleSquare[1] != -1) {
+                        boolean [] around = emptyAdjacentSquares(m, possibleSquare[0], possibleSquare[1]);
+                        dirEmptyCount[a] = 0;
+                        for(int b = 0; b < 4; b++) if(around[b]) dirEmptyCount[a]++;
+                    }
+                }
+                else dirEmptyCount[a] = 5;
+            }
+            
+            
+            int minVal = 1000, minValLoc = 0;
+            for(int a = 0; a < 4; a++) {
+                if(dirEmptyCount[a] < minVal) {
+                    minVal = dirEmptyCount[a];
+                    minValLoc = a;
+                }
+            }
+            Tron.sendMove(Tron.Direction.values()[minValLoc]);
+        }
+        
+        public static void moveFirstFree() {
+            boolean moveFound = false;
         for (int i = 1; i <= 4; i++) {
             if (isGoodMove(i)) {
                 moveFound = true;
@@ -91,4 +139,26 @@ public class TronBot {
     public static void right() {
         Tron.sendMove(Tron.Direction.EAST);
     }
+    
+    public static String stringFromDirection(int dir) {
+        return dir == 0 ? "NORTH" : dir == 1 ? "EAST" : dir == 2 ? "SOUTH" : dir == 3 ? "WEST" : "NONSENSE";
+    }
+    
+    public static boolean [] emptyAdjacentSquares(int [][] map, int locX, int locY) {
+        boolean [] empty = new boolean[4];
+        empty[0] = locY != 15 && map[locY + 1][locX] == 0;
+        empty[1] = locX != 15 && map[locY][locX + 1] == 0;
+        empty[2] = locY != 0 && map[locY - 1][locX] == 0;
+        empty[3] = locX != 0 && map[locY][locX - 1] == 0;
+        return empty;
+    }
+    
+    public static int[] getLocation(int locX, int locY, int dir) {
+        if(dir == 0) return locY == 15 ? new int[]{ -1, -1 } : new int[]{ locX, locY + 1 };
+        if(dir == 1) return locX == 15 ? new int[]{ -1, -1 } : new int[]{ locX + 1, locY };
+        if(dir == 2) return locY == 0 ? new int[]{ -1, -1 } : new int[]{ locX, locY - 1 };
+        if(dir == 3) return locX == 0 ? new int[]{ -1, -1 } : new int[]{ locX - 1, locY };
+        return new int[]{ -1, -1 };
+    }
+    
 }
--- Mini-Max/TronBot.cpp
+++ Mini-Max/TronBot.cpp
@@ -1,19 +1,18 @@
+/*
+Vaed Prasad
+AP Computer Science
+Period H
+*/
+
 #include "Tron.h"
 #include <algorithm>
 #include <set>
 #include <map>
-//std::ofstream debug;
 
-//Note: I define Standard Directional Order (which I'll call standard order) to be: NORTH, EAST, SOUTH, WEST.
-
-//Little useful function for debugging, as it will tell us what direction, in words, an integer corresponds to.
 inline std::string stringFromDirection(int dir) {
 	return dir == 0 ? "NORTH" : dir == 1 ? "EAST" : dir == 2 ? "SOUTH" : dir == 3 ? "WEST" : "NONSENSE";
 }
 
-/*This function finds out which squares adjacent to location are empty.
-It returns in the standard order of NORTH, EAST, SOUTH, and WEST as booleans which are true if the square is empty.
-Note that this function DOES create dynamic memory. Make sure to delete it when you're done.*/
 inline bool * emptyAjacentSquares(const std::vector< std::vector<int> > & map, const std::pair<int, int> & location) {
 	bool * empty = new bool[4];
 	empty[NORTH] = location.second != 15 && map[location.second + 1][location.first] == EMPTY;
@@ -23,10 +22,8 @@ inline bool * emptyAjacentSquares(const std::vector< std::vector<int> > & map, c
 	return empty;
 }
 
-//This is something to return if the square we want to return doesn't exist, and correspondingly something to compare against.
 #define BAD_LOCATION std::pair<int, int>{ -1, -1 }
 
-//This will give us the square we'd get to if we tried to move in direction dir from location.
 inline std::pair<int, int> getLocation(const std::pair<int, int> & location, int dir) {
 	if(dir == NORTH) return location.second == 15 ? BAD_LOCATION : std::pair<int, int>{ location.first, location.second + 1 };
 	if(dir == EAST) return location.first == 15 ? BAD_LOCATION : std::pair<int, int>{ location.first + 1, location.second };
@@ -35,15 +32,51 @@ inline std::pair<int, int> getLocation(const std::pair<int, int> & location, int
 	throw std::runtime_error("No such direction exists.");
 }
 
-//This is an enum which will tell us if somebody has run:
 enum Result {
 	OUR_VICTORY,
 	THEIR_VICTORY,
 	BOTH_DEATH
 };
 
+
+int main() {
+	srand(time(NULL));
+	init();
+	int turnNumber = 0;
+	while(true) {
+		turnNumber++;
+		std::vector< std::vector<int> > m = getMap();
+		std::pair<int, int> myLocation;
+		for(int y = 0; y < 16; y++)  {
+			for(int x = 0; x < 16; x++) {
+				if(m[y][x] == ME) {
+					myLocation = { x, y };
+				}
+			}
+		}
+
+		clock_t startTime = clock();
+		int results[4][4];
+		for(int a = 0; a < 4; a++) {
+			for(int b = 0; b < 4; b++) {
+				try {
+					results[a][b] = voronoiCount(simulateMove(m, a, b));
+				}
+				catch(Result r) {
+					results[a][b] = r == BOTH_DEATH ? 0 : r == THEIR_VICTORY ? -257 : 257;
+				}
+			}
+		}
+		double elapsedTime = 1000 * double(clock() - startTime) / CLOCKS_PER_SEC;
+		int miniResults[4];
+		for(int a = 0; a < 4; a++) {
+			miniResults[a] = *std::min_element(results[a], results[a] + 4);
+		}
+		sendMove(std::max_element(miniResults, miniResults + 4) - miniResults);
+	}
+}
+
 std::vector< std::vector<int> > simulateMove(std::vector< std::vector<int> > map, int myMove, int opMove) {
-	//Let's figure out where we and our opponent are:
 	std::pair<int, int> myLocation, opLocation;
 	for(int y = 0; y < 16; y++)  {
 		for(int x = 0; x < 16; x++) {
@@ -74,7 +107,6 @@ std::vector< std::vector<int> > simulateMove(std::vector< std::vector<int> > map
 
 int voronoiCount(const std::vector< std::vector<int> > & map) {
 
-	//Let's figure out where we and our opponent are:
 	std::pair<int, int> myLocation, opLocation;
 	for(int y = 0; y < 16; y++)  {
 		for(int x = 0; x < 16; x++) {
@@ -87,16 +119,12 @@ int voronoiCount(const std::vector< std::vector<int> > & map) {
 		}
 	}
 
-	/*Initialize distanceFromMe, which stores how far squares are from me.
-	Furthermore, it can also tell me whether a square has been visited by the BFS.*/
 	int distanceFromMe[16][16];
 	for(int a = 0; a < 16; a++) for(int b = 0; b < 16; b++) distanceFromMe[a][b] = 0;
 
-	//Initialize myFront to have only my location in it.
 	std::set< std::pair<int, int> > myFront;
 	myFront.insert(myLocation);
 
-	//Conduct BFS. Rather than going line by line, I'm going to explain it all here in the hopes that it's more cohesive.
 	for(unsigned char dist = 1; myFront.size() != 0; dist++) {
 		std::set< std::pair<int, int> > newFront;
 		for(auto a = myFront.begin(); a != myFront.end(); a++) {
@@ -122,16 +150,12 @@ int voronoiCount(const std::vector< std::vector<int> > & map) {
 		for(auto a = newFront.begin(); a != newFront.end(); a++) myFront.insert(*a);
 	}
 
-	/*Initialize distanceFromOp, which stores how far squares are from me.
-	Furthermore, it can also tell me whether a square has been visited by the BFS.*/
 	int distanceFromOp[16][16];
 	for(int a = 0; a < 16; a++) for(int b = 0; b < 16; b++) distanceFromOp[a][b] = 0;
 
-	//Initialize opFront to have only their location in it.
 	std::set< std::pair<int, int> > opFront;
 	opFront.insert(opLocation);
 
-	//Conduct BFS. Rather than going line by line, I'm going to explain it all here in the hopes that it's more cohesive.
 	for(unsigned char dist = 1; opFront.size() != 0; dist++) {
 		std::set< std::pair<int, int> > newFront;
 		for(auto a = opFront.begin(); a != opFront.end(); a++) {
@@ -157,9 +181,6 @@ int voronoiCount(const std::vector< std::vector<int> > & map) {
 		for(auto a = newFront.begin(); a != newFront.end(); a++) opFront.insert(*a);
 	}
 
-	/*Finally, let's go through the board and tally up which squares are closer to whom, and then use that to evaluate the position.
-	Bear in mind that a value of 0 here actually means that the square is unreachable.
-	We'll let the score be equal to the number of squares that are closer to us minus the number closer to them.*/
 	int total = 0;
 	for(int a = 0; a < 16; a++) for(int b = 0; b < 16; b++) {
 		if(distanceFromMe[a][b] == distanceFromOp[a][b]) continue;
@@ -169,64 +190,4 @@ int voronoiCount(const std::vector< std::vector<int> > & map) {
 		else total--;
 	}
 	return total;
-}
-
-int main() {
-	//Seed rand with the time.
-	srand(time(NULL));
-
-	//Initialize bot with respect to the Tron Environment.
-	init();
-
-	//We'll want to keep track of the turn number to make debugging easier.
-	int turnNumber = 0;
-
-	//Execute loop forever (or until game ends)
-	while(true) {
-		//Update turn number:
-		turnNumber++;
-
-		//Gets the newest map. Every int will have a value of EMPTY, ME, OPPONENT, TAKEN_BY_ME, or TAKEN_BY_OPPONENT.
-		std::vector< std::vector<int> > m = getMap();
-
-		//Let's figure out where we are:
-		std::pair<int, int> myLocation;
-		for(int y = 0; y < 16; y++)  {
-			for(int x = 0; x < 16; x++) {
-				if(m[y][x] == ME) {
-					myLocation = { x, y };
-				}
-			}
-		}
-
-		clock_t startTime = clock();
-		//Conduct 1-layer minimax search evaluations:
-		int results[4][4];
-		for(int a = 0; a < 4; a++) {
-			for(int b = 0; b < 4; b++) {
-				try {
-					results[a][b] = voronoiCount(simulateMove(m, a, b));
-				}
-				catch(Result r) {
-					results[a][b] = r == BOTH_DEATH ? 0 : r == THEIR_VICTORY ? -257 : 257;
-				}
-			}
-		}
-		double elapsedTime = 1000 * double(clock() - startTime) / CLOCKS_PER_SEC;
-
-		//dbg::logln("-----------------------------------------------------\nDebug for turn #" + std::to_string(turnNumber) + ':');
-		//dbg::logln("Minimax evaluation time took " + std::to_string(elapsedTime) + " milliseconds.");
-		for(int a = 0; a < 4; a++) {
-			for(int b = 0; b < 4; b++) {
-				//dbg::logln("If I move " + stringFromDirection(a) + ", and they move " + stringFromDirection(b) + ", my resulting value will be " + std::to_string(results[a][b]));
-			}
-		}
-
-		//Find minimax move.
-		int miniResults[4];
-		for(int a = 0; a < 4; a++) {
-			miniResults[a] = *std::min_element(results[a], results[a] + 4);
-		}
-		sendMove(std::max_element(miniResults, miniResults + 4) - miniResults);
-	}
 }
